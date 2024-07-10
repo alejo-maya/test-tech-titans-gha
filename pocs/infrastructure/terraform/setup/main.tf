c17967916c9bed35be572b363ef606c0854f5dd7
alejo-maya <alejo.maya@southworks.com>
2024-07-10 12:54:09 -0300

    test modified .tf

--- pocs/infrastructure/terraform/setup/main.tf
+++ pocs/infrastructure/terraform/setup/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_storage_account" "sa" {
   name                     = lower(var.naming_prefix)
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
-  account_tier             = "Standard"
+  account_tier      = "Standard"
   account_replication_type = "GRS"
 }
 
