4b8d5bc0676d47d15735a310e475e6172fcc723a
alejo-maya <alejo.maya@southworks.com>
2024-07-10 12:41:41 -0300

    test wrong indentation

--- pocs/infrastructure/terraform/setup/backend.tf
+++ pocs/infrastructure/terraform/setup/backend.tf
@@ -5,6 +5,6 @@ terraform {
     container_name        = "terraform-state"
     key                   = "sw-tech-titans-pocue-1-tfstate-rg001.tfstate"
     subscription_id       = "a5a86e7a-a5a4-4a0f-98b6-605001cf22b2"
-    tenant_id             = "b25036e3-de39-4fec-a4aa-bda41b870d38"
+    tenant_id            = "b25036e3-de39-4fec-a4aa-bda41b870d38"
   }
 }
