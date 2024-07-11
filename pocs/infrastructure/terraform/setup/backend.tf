0ad24fe03c95a0b63ff88387bfc36e803cb3a47b
alejo-maya <alejo.maya@southworks.com>
2024-07-11 15:19:57 -0300

    Update backend.tf

--- pocs/infrastructure/terraform/setup/backend.tf
+++ pocs/infrastructure/terraform/setup/backend.tf
@@ -1,11 +1,11 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "sw-tech-titans-pocue-1-tfstate-rg001"
-    storage_account_name = "swttpoc1tfstate001"
-    container_name       = "terraform-state"
-    key                  = "sw-tech-titans-pocue-1-tfstate-rg001.tfstate"
-    subscription_id      = "a5a86e7a-a5a4-4a0f-98b6-605001cf22b2"
-    tenant_id            = "b25036e3-de39-4fec-a4aa-bda41b870d38"
-    use_oidc             = true
-  }
-}
+#   backend "azurerm" {
+#     resource_group_name  = "sw-tech-titans-pocue-1-tfstate-rg001"
+#     storage_account_name = "swttpoc1tfstate001"
+#     container_name       = "terraform-state"
+#     key                  = "sw-tech-titans-pocue-1-tfstate-rg001.tfstate"
+#     subscription_id      = "a5a86e7a-a5a4-4a0f-98b6-605001cf22b2"
+#     tenant_id            = "b25036e3-de39-4fec-a4aa-bda41b870d38"
+#     use_oidc             = true
+#   }
+# }
